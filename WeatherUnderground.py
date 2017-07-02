e255bf6b0d14a83c3514159672c3652fab98dacc
Hubby-G <earthling471@gmail.com>
2017-07-02 17:16:06 -0600

    intial commit - Weather Underground script

---
+++ WeatherUnderground.py
@@ -0,0 +1,310 @@
+#!/usr/bin/python
+
+# 30 Jun 17
+# code  Weather-pi-station   collected from http://makezine.com/projects/raspberry-pi-weather-station-mount/
+# and  bme280.py from
+# add Pi collected temperature data to weatherunderground.com
+
+
+from __future__ import print_function
+
+import smbus
+import sqlite3
+import datetime
+import os
+import sys
+import time
+import urllib2
+from urllib import urlencode
+from ctypes import c_short
+from ctypes import c_byte
+from ctypes import c_ubyte
+from config import Config
+
+# ============================================================================
+# Constants
+# ============================================================================
+#dbName = "/var/www/BeeHive.db"
+DEVICE = 0x76 # Default device I2C address
+conn = sqlite3.connect('farm2017.db')
+c = conn.cursor()
+#camera = PiCamera()
+
+bus = smbus.SMBus(1) # Rev 2 Pi, Pi 2 & Pi 3 uses bus 1
+                     # Rev 1 Pi uses bus 0
+
+# specifies how often to measure values from the Sense HAT (in minutes)
+MEASUREMENT_INTERVAL = 10  # minutes
+
+# Set to False when testing the code and/or hardware
+# Set to True to enable upload of weather data to Weather Underground
+WEATHER_UPLOAD = False
+
+# the weather underground URL used to upload weather data
+WU_URL = "http://weatherstation.wunderground.com/weatherstation/updateweatherstation.php"
+
+c.execute("CREATE TABLE IF NOT EXISTS (unixT INT, TempC FLOAT, humidity FLOAT, pressure FLOAT)")
+
+# some string constants
+SINGLE_HASH = "#"
+HASHES = "########################################"
+SLASH_N = "\n"
+
+def get_cpu_temp():
+    # 'borrowed' from https://www.raspberrypi.org/forums/viewtopic.php?f=104&t=111457
+    # executes a command at the OS to pull in the CPU temperature
+    res = os.popen('vcgencmd measure_temp').readline()
+    return float(res.replace("temp=", "").replace("'C\n", ""))
+
+#def get_temp():
+def getShort(data, index):
+  # return two bytes from data as a signed 16-bit value
+  return c_short((data[index+1] << 8) + data[index]).value
+
+def getUShort(data, index):
+  # return two bytes from data as an unsigned 16-bit value
+  return (data[index+1] << 8) + data[index]
+
+def getChar(data,index):
+  # return one byte from data as a signed char
+  result = data[index]
+  if result > 127:
+    result -= 256
+  return result
+
+def getUChar(data,index):
+  # return one byte from data as an unsigned char
+  result =  data[index] & 0xFF
+  return result
+
+def readBME280ID(addr=DEVICE):
+  # Chip ID Register Address
+  REG_ID     = 0xD0
+  (chip_id, chip_version) = bus.read_i2c_block_data(addr, REG_ID, 2)
+  return (chip_id, chip_version)
+
+def readBME280All(addr=DEVICE):
+  # Register Addresses
+  REG_DATA = 0xF7
+  REG_CONTROL = 0xF4
+  REG_CONFIG  = 0xF5
+
+  REG_CONTROL_HUM = 0xF2
+  REG_HUM_MSB = 0xFD
+  REG_HUM_LSB = 0xFE
+
+  # Oversample setting - page 27
+  OVERSAMPLE_TEMP = 2
+  OVERSAMPLE_PRES = 2
+  MODE = 1
+
+  # Oversample setting for humidity register - page 26
+  OVERSAMPLE_HUM = 2
+  bus.write_byte_data(addr, REG_CONTROL_HUM, OVERSAMPLE_HUM)
+
+  control = OVERSAMPLE_TEMP<<5 | OVERSAMPLE_PRES<<2 | MODE
+  bus.write_byte_data(addr, REG_CONTROL, control)
+
+  # Read blocks of calibration data from EEPROM
+  # See Page 22 data sheet
+  cal1 = bus.read_i2c_block_data(addr, 0x88, 24)
+  cal2 = bus.read_i2c_block_data(addr, 0xA1, 1)
+  cal3 = bus.read_i2c_block_data(addr, 0xE1, 7)
+
+  # Convert byte data to word values
+  dig_T1 = getUShort(cal1, 0)
+  dig_T2 = getShort(cal1, 2)
+  dig_T3 = getShort(cal1, 4)
+
+  dig_P1 = getUShort(cal1, 6)
+  dig_P2 = getShort(cal1, 8)
+  dig_P3 = getShort(cal1, 10)
+  dig_P4 = getShort(cal1, 12)
+  dig_P5 = getShort(cal1, 14)
+  dig_P6 = getShort(cal1, 16)
+  dig_P7 = getShort(cal1, 18)
+  dig_P8 = getShort(cal1, 20)
+  dig_P9 = getShort(cal1, 22)
+
+  dig_H1 = getUChar(cal2, 0)
+  dig_H2 = getShort(cal3, 0)
+  dig_H3 = getUChar(cal3, 2)
+
+  dig_H4 = getChar(cal3, 3)
+  dig_H4 = (dig_H4 << 24) >> 20
+  dig_H4 = dig_H4 | (getChar(cal3, 4) & 0x0F)
+
+  dig_H5 = getChar(cal3, 5)
+  dig_H5 = (dig_H5 << 24) >> 20
+  dig_H5 = dig_H5 | (getUChar(cal3, 4) >> 4 & 0x0F)
+
+  dig_H6 = getChar(cal3, 6)
+
+  # Wait in ms (Datasheet Appendix B: Measurement time and current calculation)
+  wait_time = 1.25 + (2.3 * OVERSAMPLE_TEMP) + ((2.3 * OVERSAMPLE_PRES) + 0.575) + ((2.3 * OVERSAMPLE_HUM)+0.575)
+  time.sleep(wait_time/1000)  # Wait the required time
+
+  # Read temperature/pressure/humidity
+  data = bus.read_i2c_block_data(addr, REG_DATA, 8)
+  pres_raw = (data[0] << 12) | (data[1] << 4) | (data[2] >> 4)
+  temp_raw = (data[3] << 12) | (data[4] << 4) | (data[5] >> 4)
+  hum_raw = (data[6] << 8) | data[7]
+
+  #Refine temperature
+  var1 = ((((temp_raw>>3)-(dig_T1<<1)))*(dig_T2)) >> 11
+  var2 = (((((temp_raw>>4) - (dig_T1)) * ((temp_raw>>4) - (dig_T1))) >> 12) * (dig_T3)) >> 14
+  t_fine = var1+var2
+  temperature = float(((t_fine * 5) + 128) >> 8);
+
+  # Refine pressure and adjust for temperature
+  var1 = t_fine / 2.0 - 64000.0
+  var2 = var1 * var1 * dig_P6 / 32768.0
+  var2 = var2 + var1 * dig_P5 * 2.0
+  var2 = var2 / 4.0 + dig_P4 * 65536.0
+  var1 = (dig_P3 * var1 * var1 / 524288.0 + dig_P2 * var1) / 524288.0
+  var1 = (1.0 + var1 / 32768.0) * dig_P1
+  if var1 == 0:
+    pressure=0
+  else:
+    pressure = 1048576.0 - pres_raw
+    pressure = ((pressure - var2 / 4096.0) * 6250.0) / var1
+    var1 = dig_P9 * pressure * pressure / 2147483648.0
+    var2 = pressure * dig_P8 / 32768.0
+    pressure = pressure + (var1 + var2 + dig_P7) / 16.0
+#    pressure = float("{:0.2f}".format(pressure))
+
+  # Refine humidity
+  humidity = t_fine - 76800.0
+  humidity = (hum_raw - (dig_H4 * 64.0 + dig_H5 / 16384.0 * humidity)) * (dig_H2 / 65536.0 * (1.0 + dig_H6 / 67108864.0 * humidity * (1.0 + dig_H3 / 67108864.0 * humidity)))
+  humidity = humidity * (1.0 - dig_H1 * humidity / 524288.0)
+  humidity = float("{:0.1f}".format(humidity))
+  if humidity > 100:
+    humidity = 100
+  elif humidity < 0:
+    humidity = 0
+
+  return temperature/100.0,pressure/100.0,humidity
+
+def main():
+    temperature,pressure,humidity = readBME280All()
+    press = "{:0.2f}".format(pressure)
+    hum = "{:0.1f}".format(humidity)
+
+    global last_temp
+
+    # initialize the lastMinute variable to the current time to start
+    last_minute = datetime.datetime.now().minute
+    # on startup, just use the previous minute as lastMinute
+    last_minute -= 1
+    if last_minute == 0:
+        last_minute = 59
+
+    # infinite loop to continuously check weather values
+    while 1:
+        # The temp measurement smoothing algorithm's accuracy is based
+        # on frequent measurements, so we'll take measurements every 5 seconds
+        # but only upload on measurement_interval
+        current_second = datetime.datetime.now().second
+        # are we at the top of the minute or at a 5 second interval?
+        if (current_second == 0) or ((current_second % 5) == 0):
+            # ========================================================
+            # read values from BME-280
+            # ========================================================
+
+            # get the current minute
+            current_minute = datetime.datetime.now().minute
+            # is it the same minute as the last time we checked?
+            if current_minute != last_minute:
+                # reset last_minute to the current_minute
+                last_minute = current_minute
+                # is minute zero, or divisible by 10?
+                # we're only going to take measurements every MEASUREMENT_INTERVAL minutes
+                if (current_minute == 0) or ((current_minute % MEASUREMENT_INTERVAL) == 0):
+                    # get the reading timestamp
+                    now = datetime.datetime.now()
+                    print("\n%d minute mark (%d @ %s)" % (MEASUREMENT_INTERVAL, current_minute, str(now)))
+
+                    # ========================================================
+                    # Upload the weather data to Weather Underground
+                    # ========================================================
+                    # is weather upload enabled (True)?
+                    if WEATHER_UPLOAD:
+                        # From http://wiki.wunderground.com/index.php/PWS_-_Upload_Protocol
+                        print("Uploading data to Weather Underground")
+
+                        t = str (temperature)
+                        h = str (hum)
+                        p = str (press)
+                        print ("Temperature : " + t + "C")
+                        print ("Pressure : " + p + "hPa")
+                        print ("Humidity : " + h + "%"+'\n' + '------------------------------------')
+                        c.execute("INSERT INTO hoophouse2017 (unixT, TempC, humidity, pressure) VALUES (?, ?, ?, ?)",
+                            (unixT, temperature, hum, press))
+                        conn.commit()
+
+                        # build a weather data object
+                        weather_data = {
+                            "action": "updateraw",
+                            "ID": wu_station_id,
+                            "PASSWORD": wu_station_key,
+                            "dateutc": "now",
+                            "temp": str(temp),
+                            "humidity": str(humidity),
+                            "baromin": str(pressure),
+                        }
+                        try:
+                            upload_url = WU_URL + "?" + urlencode(weather_data)
+                            response = urllib2.urlopen(upload_url)
+                            html = response.read()
+                            print("Server response:", html)
+                            # do something
+                            response.close()  # best practice to close the file
+                        except:
+                            print("Exception:", sys.exc_info()[0], SLASH_N)
+                    else:
+                        print("Skipping Weather Underground upload")
+
+        # wait a second then check again
+        # You can always increase the sleep value below to check less often
+        time.sleep(1)  # this should never happen since the above is an infinite loop
+
+    print("Leaving main()")
+
+
+# ============================================================================
+# here's where we start doing stuff
+# ============================================================================
+print(SLASH_N + HASHES)
+print(SINGLE_HASH, "Pi Weather Station                  ", SINGLE_HASH)
+print(SINGLE_HASH, "By John M. Wargo (www.johnwargo.com)", SINGLE_HASH)
+print(HASHES)
+
+# make sure we don't have a MEASUREMENT_INTERVAL > 60
+if (MEASUREMENT_INTERVAL is None) or (MEASUREMENT_INTERVAL > 60):
+    print("The application's 'MEASUREMENT_INTERVAL' cannot be empty or greater than 60")
+    sys.exit(1)
+
+# ============================================================================
+#  Read Weather Underground Configuration Parameters
+# ============================================================================
+print("\nInitializing Weather Underground configuration")
+wu_station_id = IPARKLAN14
+wu_station_key = p1rgpw5r
+if (wu_station_id is None) or (wu_station_key is None):
+    print("Missing values from the Weather Underground configuration file\n")
+    sys.exit(1)
+
+# we made it this far, so it must have worked...
+print("Successfully read Weather Underground configuration values")
+print("Station ID:", wu_station_id)
+# print("Station key:", wu_station_key)
+
+print("Initialization complete!")
+
+# Now see what we're supposed to do next
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("\nExiting application\n")
+sys.exit(0)
